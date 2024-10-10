e5fe9bd7ac97387fad5cdbea0d17d1910bc6b11e
Rodrigo <rodrigocid2002@gmail.com>
2024-10-10 13:01:48 +0200

    Botones y Toast hechos.

--- app/src/main/java/com/example/rodrigo_cid_colino_pmdm_tarea1/MainActivity.java
+++ app/src/main/java/com/example/rodrigo_cid_colino_pmdm_tarea1/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.rodrigo_cid_colino_pmdm_tarea1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,33 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        Button bLogin = findViewById(R.id.btIniciar);
+        EditText tfUser = findViewById(R.id.ptNombre);
+        EditText tfPass = findViewById(R.id.ptPassword);
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                if (tfUser.getText().toString().equals("admin") && tfPass.getText().toString().equals("admin")) {
+
+
+                } else {
+
+                    //Make a toast here with the message "hello"
+                    Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
+
+                }
+
+
+            }
+
+
+        });
+
+
     }
 }
